e2ff9d1a21fd090c116fe098e71a3f1551798985
housekjh1 <141015592+housekjh1@users.noreply.github.com>
2024-03-16 16:43:44 +0900

    슬라이딩 윈도우 문제 풀이

---
+++ 03 challenge/findLongestSubstring.js
@@ -0,0 +1,26 @@
+// Sliding Window - findLongestSubstring
+// 문자열을 받아 모든 고유 문자가 포함된 가장 긴 하위 문자열의 길이를 반환하는 findLongestSubstring이라는 함수를 작성하세요.
+
+function findLongestSubstring(str) {
+    let longest = 0;
+    let seen = {};
+    let start = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (seen[char]) {
+            start = Math.max(start, seen[char]);
+        }
+        longest = Math.max(longest, i - start + 1);
+        seen[char] = i + 1;
+    }
+    return console.log(longest);
+}
+
+findLongestSubstring('') // 0
+findLongestSubstring('rithmschool') // 7
+findLongestSubstring('thisisawesome') // 6
+findLongestSubstring('thecatinthehat') // 7
+findLongestSubstring('bbbbbb') // 1
+findLongestSubstring('longestsubstring') // 8
+findLongestSubstring('thisishowwedoit') // 6
